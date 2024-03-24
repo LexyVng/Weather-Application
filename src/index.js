349f19f380fdda852e43db4becac3b669d747d2f
LexyVng <163355297+LexyVng@users.noreply.github.com>
2024-03-24 14:01:07 +0000

    Added search functionality with API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
+function updateWeatherData(response) {
+  let cityElement = document.querySelector("#current-city");
+  let countryElement = document.querySelector("#current-country");
+  let temperatureElement = document.querySelector("#current-temp-value");
+
+  let temperature = response.data.temperature.current;
+  let country = response.data.country;
+  let newCity = response.data.city;
+
+  cityElement.innerHTML = newCity;
+  countryElement.innerHTML = `, ${country}`;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "2b4a0533t1055afa3fbo41efac5059ad";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let inputElement = document.querySelector("#weather-app-form-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = inputElement.value;
+
+  searchCity(inputElement.value);
 }
 
 let formElement = document.querySelector("#weather-app-form");
 formElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Athens");
